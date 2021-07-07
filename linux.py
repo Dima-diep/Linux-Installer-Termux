f86ebbb5fe7013de3b70d005dd5ff9be92d183d0
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-07 11:04:45 +0400

    linux.py

--- linux.py
+++ linux.py
@@ -42,34 +42,21 @@ a = int(input())
 
 os.system("clear")
 time.sleep(2.5)
-def loading() -> None:
-    load = ["\", "|", "/", "-"]
-    while True:
-        for p in load:
-            print("Setting up apt... {p}")
-            sleep(0.5)
-            os.system("clear")
-def loading() -> None:
-    load = ["\", "|", "/", "-"]
-    while True:
-        for p in load:
-            print("Setting up apt... {p}")
-            sleep(0.5)
-            os.system("clear")
-def loading() -> None:
-    load = ["\", "|", "/", "-"]
-    while True:
-        for p in load:
-            print("Setting up apt... {p}")
-            sleep(0.5)
-            os.system("clear")
+for i in range(0,3):
+    def loading() -> None:
+        load = ["\", "|", "/", "-"]
+        while True:
+            for p in load:
+                print("Setting up apt... {p}")
+                sleep(0.5)
+                os.system("clear")
 time.sleep(2.5)
 os.system("clear")
 
 if a == 1:
     os.system("clear")
     time.sleep(5)
-    os.system("toilet -F crop -F border KALI | lolcat -S 55 -a -s 40")
+    os.system("toilet -F crop -F border KALI | lolcat -S 55 -a -s 40 -i")
     time.sleep(1.25)
     print("Installing Kali subsystem Termux...")
     time.sleep(1.25)
@@ -77,7 +64,7 @@ if a == 1:
 elif a == 2:
     os.system("clear")
     time.sleep(5)
-    os.system("toilet -F crop -F border ubuntu | lolcat -S 30 -a -s 40")
+    os.system("toilet -F crop -F border ubuntu | lolcat -S 30 -a -s 40 -i")
     time.sleep(1.25)
     print("Installing Ubuntu 20.04 LTS subsystem Termux...")
     time.sleep(1.25)
@@ -85,7 +72,7 @@ elif a == 2:
 elif a == 3:
     os.system("clear")
     time.sleep(5)
-    os.system("cd ~ && toilet -F crop -F border DEBIAN | lolcat -S 40 -a -s 40")
+    os.system("cd ~ && toilet -F crop -F border DEBIAN | lolcat -S 40 -a -s 40 -i")
     time.sleep(1.25)
     print("Installing Debian subsystem Termux...")
     time.sleep(1.25)
@@ -109,14 +96,14 @@ elif a == 5:
 elif a == 6:
     os.system("clear")
     time.sleep(5)
-    os.system("toilet -F crop -F border Fedora 33 | lolcat -S 62 -a -s 40")
+    os.system("toilet -F crop -F border Fedora 33 | lolcat -S 62 -a -s 40 -i")
     time.sleep(1.25)
     print("Installing Fedora subsystem Termux...")
     os.system("cd ~ && pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Fedora/fedora.sh | bash && chmod +x * && ./start-fedora.sh")
 elif a == 7:
     os.system("clear")
     time.sleep(5)
-    os.system("toilet -F crop -F border void | lolcat -S 72 -a -s 40")
+    os.system("toilet -F crop -F border void | lolcat -S 72 -a -s 40 -i")
     time.sleep(1.25)
     print("Installing void subsystem Termux...")
     time.sleep(1.25)
@@ -124,7 +111,7 @@ elif a == 7:
 elif a == 8:
     os.system("clear")
     time.sleep(5)
-    os.system("toilet -F crop -F border Alpine | lolcat -S 55 -a -s 40")
+    os.system("toilet -F crop -F border Alpine | lolcat -S 55 -a -s 40 -i")
     time.sleep(1.25)
     print("Installing Alpine subsystem Termux...")
     time.sleep(1.25)
@@ -140,7 +127,7 @@ elif a == 9:
 elif a == 10:
     os.system("clear")
     time.sleep(5)
-    os.system("figlet -f big CentOS | lolcat -S 50 -a -s 40")
+    os.system("figlet -f big CentOS | lolcat -S 50 -a -s 40 -i")
     time.sleep(1.25)
     print("Installing CentOS subsystem Termux...")
     time.sleep(1.25)
@@ -148,7 +135,7 @@ elif a == 10:
 elif a == 11:
     os.system("clear")
     time.sleep(5)
-    os.system("figlet -f big openSUSE | lolcat -S 1 -a -s 40")
+    os.system("figlet -f big openSUSE | lolcat -S 1 -a -s 40 -i")
     time.sleep(1.25)
     print("Installing openSUSE Leap subsystem Termux...")
     time.sleep(1.25)
@@ -156,7 +143,7 @@ elif a == 11:
 elif a == 12:
     os.system("clear")
     time.sleep(5)
-    os.system("figlet -f big openSUSE | lolcat -S 1 -a -s 40")
+    os.system("figlet -f big openSUSE | lolcat -S 1 -a -s 40 -i")
     time.sleep(1.25)
     print("Installing openSUSE Tumbleweed subsystem Termux...")
     time.sleep(1.25)
